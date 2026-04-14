94ab35b1abf84251203731d064fa02199bbaf9fd
diogotvf7 <diogotvf7@gmail.com>
2026-04-14 01:20:20 +0100

    Added NextJs skeleton for frontend

--- backend/orchestrator-service/src/main/java/com/autocurate/spotify/clustering/controller/AuthController.java
+++ backend/orchestrator-service/src/main/java/com/autocurate/spotify/clustering/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.autocurate.spotify.clustering.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import se.michaelthelin.spotify.SpotifyApi;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     @Autowired
--- backend/orchestrator-service/src/main/java/com/autocurate/spotify/clustering/controller/SpotifyController.java
+++ backend/orchestrator-service/src/main/java/com/autocurate/spotify/clustering/controller/SpotifyController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.autocurate.spotify.clustering.service.SpotifySyncService;
 
 @RestController
 @RequestMapping("/api/spotify")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SpotifyController {
 
     @Autowired
